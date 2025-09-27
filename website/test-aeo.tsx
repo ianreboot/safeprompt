b19c04314ad81a91f92c9ab4e21a2cd60856b3c4
Ian Ho <ian.ho@rebootmedia.net>
2025-09-27 02:51:20 +0000

    Clean up temporary files

--- website/test-aeo.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import AEOLayout from '@/components/blog/AEOLayout'
-export default function Test() {
-  return <AEOLayout title="Test" metadata={{ directAnswer: "test", lastUpdated: "2025", quickFacts: [] }}>Test</AEOLayout>
-}
